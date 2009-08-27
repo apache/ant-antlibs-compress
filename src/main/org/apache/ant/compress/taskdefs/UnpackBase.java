471834f3391b281b08822aae3a4b9b5b2970981d
Stefan Bodewig <bodewig@apache.org>
2009-08-27 04:18:11 +0000

    gunzip and bunzip2 tasks
    git-svn-id: https://svn.apache.org/repos/asf/ant/sandbox/antlibs/compress/trunk@808278 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/org/apache/ant/compress/taskdefs/UnpackBase.java
@@ -0,0 +1,93 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package org.apache.ant.compress.taskdefs;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ant.compress.util.CompressorStreamFactory;
+import org.apache.commons.compress.compressors.CompressorInputStream;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.util.FileUtils;
+import org.apache.tools.ant.taskdefs.Unpack;
+
+/**
+ * Expands a resource that has been compressed.
+ *
+ */
+public abstract class UnpackBase extends Unpack {
+    private static final int BUFFER_SIZE = 8 * 1024;
+
+    private final String defaultExtension;
+    private final CompressorStreamFactory factory;
+
+    protected UnpackBase(String defaultExtension,
+                         CompressorStreamFactory factory) {
+        this.defaultExtension = defaultExtension;
+        this.factory = factory;
+    }
+
+    /**
+     * Get the default extension.
+     */
+    protected final String getDefaultExtension() {
+        return defaultExtension;
+    }
+
+    /**
+     * Implement the gunzipping.
+     */
+    protected void extract() {
+        if (source.lastModified() > dest.lastModified()) {
+            log("Expanding " + source.getAbsolutePath() + " to "
+                + dest.getAbsolutePath());
+
+            FileOutputStream out = null;
+            CompressorInputStream zIn = null;
+            InputStream fis = null;
+            try {
+                out = new FileOutputStream(dest);
+                fis = srcResource.getInputStream();
+                zIn = factory.getCompressorStream(new BufferedInputStream(fis));
+                byte[] buffer = new byte[BUFFER_SIZE];
+                int count = 0;
+                do {
+                    out.write(buffer, 0, count);
+                    count = zIn.read(buffer, 0, buffer.length);
+                } while (count != -1);
+            } catch (IOException ioe) {
+                String msg = "Problem expanding " + ioe.getMessage();
+                throw new BuildException(msg, ioe, getLocation());
+            } finally {
+                FileUtils.close(fis);
+                FileUtils.close(out);
+                FileUtils.close(zIn);
+            }
+        }
+    }
+
+    /**
+     * Yes, we can.
+     */
+    protected final boolean supportsNonFileResources() {
+        return true;
+    }
+}
---
+++ src/main/org/apache/ant/compress/util/BZip2StreamFactory.java
@@ -0,0 +1,51 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package org.apache.ant.compress.util;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.commons.compress.compressors.CompressorInputStream;
+import org.apache.commons.compress.compressors.CompressorOutputStream;
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorOutputStream;
+
+/**
+ * Creates streams for the supported compression formats.
+ */
+public class BZip2StreamFactory implements CompressorStreamFactory {
+
+    /**
+     * @param stream the stream to read from, should be buffered
+     */
+    public CompressorInputStream getCompressorStream(InputStream stream)
+        throws IOException {
+        return new BZip2CompressorInputStream(stream);
+    }
+
+
+    /**
+     * @param stream the stream to write to, should be buffered
+     */
+    public CompressorOutputStream getCompressorStream(OutputStream stream)
+        throws IOException {
+        return new BZip2CompressorOutputStream(stream);
+    }
+}
