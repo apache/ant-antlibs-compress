ce554cb93c4a3c0d37a8295ec9c06a2594fc12c6
Stefan Bodewig <bodewig@apache.org>
2011-11-05 06:47:31 +0000

    add support for the XZ format.  Requires Commons Compress trunk and XZ for Java
    git-svn-id: https://svn.apache.org/repos/asf/ant/antlibs/compress/trunk@1197895 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/org/apache/ant/compress/taskdefs/XZ.java
@@ -0,0 +1,41 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package org.apache.ant.compress.taskdefs;
+
+import org.apache.ant.compress.resources.CommonsCompressCompressorResource;
+import org.apache.ant.compress.resources.XZResource;
+import org.apache.ant.compress.util.XZStreamFactory;
+import org.apache.tools.ant.types.Resource;
+
+/**
+ * Compresses using XZ.
+ * @since Apache Compress Antlib 1.2
+ */
+public final class XZ extends PackBase {
+
+    public XZ() {
+        super(new XZStreamFactory(),
+              new PackBase.ResourceWrapper() {
+                public CommonsCompressCompressorResource wrap(Resource dest) {
+                    return new XZResource(dest);
+                }
+            });
+    }
+
+}
